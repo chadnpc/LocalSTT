e6417ac238af7b35c51a27c46ec33aca6e5fd4e0
Alain Herve <alainQtec@gmail.com>
2025-01-21 04:14:19 +0200

    Update Receive-Audio.ps1

--- Public/Receive-Audio.ps1
+++ Public/Receive-Audio.ps1
@@ -11,7 +11,7 @@ function Receive-Audio {
   # .EXAMPLE
   #   Record-Audio -o ~/output.wav
   #   Will record audio and save it to the specified path (WIP)
-  [CmdletBinding()][Alias('Record-Audio')]
+  [CmdletBinding()][OutputType([System.IO.FileInfo])][Alias('Record-Audio')]
   param (
     [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true)]
     [ValidateScript({
@@ -33,7 +33,7 @@ function Receive-Audio {
   )
   begin {
     $params = $PSCmdlet.MyInvocation.BoundParameters
-    $_ofile = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($_ofile)
+    $_ofile = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($OutFile)
   }
   process {
     $rec = $params.ContainsKey('OutFile') ? ([LocalSTT]::RecordAudio($_ofile)) : ([LocalSTT]::RecordAudio())
