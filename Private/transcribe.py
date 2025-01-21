bb2cc04d56a0ef2af5e5e5e425622b3217941f19
Alain Herve <alainQtec@gmail.com>
2025-01-22 01:48:21 +0200

    Update transcribe.py

--- Private/transcribe.py
+++ Private/transcribe.py
@@ -14,33 +14,25 @@ model_size: str = "large-v3"
 stop_event: threading.Event = threading.Event()
 
 def get_text_from_audio(input_audio_file: str, output_text_file: str) -> None:
-    # or run on CPU with INT8
+    # run on CPU with INT8
     model = WhisperModel(model_size, device="cpu", compute_type="int8")
-    print("• transcribing...")
+    print(f"  ● Transcribing '{input_audio_file}'...")
     segments, info = model.transcribe(input_audio_file, beam_size=5)
-    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+    print("  ┆ Detected language '%s' with P(A): %f" % (info.language.upper(), info.language_probability))
 
     with open(output_text_file, "w") as outfile:
         for segment in segments:
-            text_to_print = "[%.2fs -> %.2fs] %s\n" % (
-                segment.start,
-                segment.end,
-                segment.text,
-            )
-            print(text_to_print.strip())
+            # For debug purposes:
+            # text_to_print = "[%.2fs -> %.2fs] %s\n" % (
+            #     segment.start,
+            #     segment.end,
+            #     segment.text,
+            # )
+            # print(text_to_print.strip())
             outfile.write(segment.text)
-
-    print("• transcription done!")
+    print("  ● done.")
+    return
 
 if __name__ == "__main__":
-    transcribe_thread = threading.Thread(target=get_text_from_audio, args=(args.inputfile, args.outfile))
-    transcribe_thread.start()
-
-    try:
-      while not stop_event.is_set():
-        time.sleep(0.1)  # Short delay to prevent busy waiting
-    except KeyboardInterrupt:
-      stop_event.set()
-    finally:
-      transcribe_thread.join()
-    exit(0)
+  get_text_from_audio(args.inputfile, args.outfile)
+  exit(0)
