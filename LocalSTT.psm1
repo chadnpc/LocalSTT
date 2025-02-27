ecd2ef4c8d36f775eb70b842fdadd11973bebdcb
alain <alainQtec@gmail.com>
2025-02-27 10:12:15 +0200

    Update LocalSTT.psm1

--- LocalSTT.psm1
+++ LocalSTT.psm1
@@ -5,7 +5,7 @@ using namespace System.Net.Sockets
 using namespace System.Collections.Generic
 using namespace System.Management.Automation
 
-#Requires -Modules cliHelper.core, cliHelper.errorman, pipEnv
+#Requires -Modules pipEnv, cliHelper.core
 #Requires -Psedition Core
 
 #region    Classes
@@ -46,25 +46,24 @@ class AudioRecorder {
     [LocalSTT]::data.IsRecording = $false
   }
 }
+
 class PyenvHelper {
-  PyenvHelper() {}
-  static [void] useversion([string]$ver) {
-    [PyenvHelper]::useversion([version]::new($ver), [ref][LocalSTT]::data)
+  PyenvHelper() {
+    $this.PsObject.Properties.Add([psscriptproperty]::new('pyversion', { $v = (&"/$(Get-Variable HOME -ValueOnly)/.pyenv/shims/python" --version).Split(" ")[1]; return [version]::new($v) }, { throw 'pyversion is read-only' }))
   }
-  static [void] useversion([version]$ver, [ref]$config) {
-    [version]$current_ver = [PyenvHelper]::get_python_version()
-    if ($current_ver -lt $ver) {
+  [void] Useversion([string]$ver) {
+    $this.Useversion([version]::new($ver), [ref][LocalSTT]::data)
+  }
+  [void] Useversion([version]$ver, [ref]$config) {
+    if ($this.pyversion -lt $ver) {
       Write-Console "Installing Python v$ver..." -f SlateBlue
       pyenv install $ver.ToString()
     }
     pyenv local "$ver"
     $config.Value.Env.PythonVersion = $ver
-    if ($current_ver -ne $ver) {
+    if ($this.pyversion -ne $ver) {
       throw [InvalidOperationException]::new("Failed to set Python version to $ver")
     }
-  }
-  static [version] get_python_version() {
-    return [version]::new((python --version).Split(" ")[1])
   }
 }
 
@@ -80,25 +79,25 @@ class PyenvHelper {
 #   https://pyaudio.readthedocs.io/en/stable/
 class LocalSTT : ThreadRunner {
   static [AudioRecorder] $recorder
-  static [PsRecord]$data = [LocalSTT]::GetSttData()
+  static [PsRecord]$data = (Get-Config)
   LocalSTT() {}
 
   static [IO.Fileinfo] RecordAudio() { return [LocalSTT]::RecordAudio(3) }
 
   static [IO.Fileinfo] RecordAudio([float]$minutes) {
-    return [LocalSTT]::RecordAudio([LocalSTT]::data.Stt.outFile, [Timespan]::new(0, 0, $minutes * 60))
+    return [LocalSTT]::RecordAudio([LocalSTT]::data.Server.outFile, [Timespan]::new(0, 0, $minutes * 60))
   }
   static [IO.Fileinfo] RecordAudio([string]$outFile, [float]$minutes) {
     return [LocalSTT]::RecordAudio($outFile, [Timespan]::new(0, 0, $minutes * 60))
   }
   static [IO.Fileinfo] RecordAudio([string]$outFile, [Timespan]$duration) {
     [ValidateNotNullOrWhiteSpace()][string]$outFile = $outFile
-    if ([LocalSTT]::IsFirstRun()) { [void][LocalSTT]::ResolveRequirements() };
+    if (IsFirstRun) { [void](Resolve-Requirements) };
     if ([LocalSTT]::data.Env.State -eq "inactive") {
       [LocalSTT]::data.Env.Activate()
     }
-    $_c = [LocalSTT].config; [LocalSTT]::recorder = [AudioRecorder]::New([TcpListener]::new([IPEndpoint]::new([IPAddress]$_c.Stt.host, $_c.Stt.port)), [IO.FileInfo]::New($outFile)); $dir = $_c.Stt.workingDirectory
-    [LocalSTT]::data.Process = Start-Process -FilePath "python" -ArgumentList "$($_c.Stt.Script) --host `"$($_c.Stt.host)`" --port $($_c.Stt.port) --amplify-rate $($_c.Stt.amplifyRate) --outfile `"$outFile`" --duration-in-minutes=$($duration.TotalMinutes) --working-directory `"$dir`"" -WorkingDirectory $dir -PassThru -NoNewWindow
+    $_c = [LocalSTT].config; [LocalSTT]::recorder = [AudioRecorder]::New([TcpListener]::new([IPEndpoint]::new([IPAddress]$_c.Server.host, $_c.Server.port)), [IO.FileInfo]::New($outFile)); $dir = $_c.Server.workingDirectory
+    [LocalSTT]::data.Process = Start-Process -FilePath "python" -ArgumentList "$($_c.Server.Script) --host `"$($_c.Server.host)`" --port $($_c.Server.port) --amplify-rate $($_c.Server.amplifyRate) --outfile `"$outFile`" --duration-in-minutes=$($duration.TotalMinutes) --working-directory `"$dir`"" -WorkingDirectory $dir -PassThru -NoNewWindow
     Write-Console "(LocalSTT) " -f SlateBlue -NoNewLine; Write-Console "▶︎ Recording server starting @ http://$([LocalSTT]::recorder.listener.LocalEndpoint) PID: $([LocalSTT]::data.Process.Id)" -f LemonChiffon;
     $OgctrInput = [Console]::TreatControlCAsInput;
     try {
@@ -126,91 +125,16 @@ class LocalSTT : ThreadRunner {
     [ValidateNotNullOrEmpty()][IO.FileInfo]$InputAudio = $InputAudio;
     [string]$inputFile = $InputAudio.FullName;
     if (!$InputAudio.Exists) { throw [FileNotFoundException]::New("Could Not Find Audio File $inputFile") }
-    if ([LocalSTT]::IsFirstRun()) { [void][LocalSTT]::ResolveRequirements() }
+    if (IsFirstRun) { [void](Resolve-Requirements) }
     if ([LocalSTT]::data.Env.State -eq "inactive") {
       [LocalSTT]::data.Env.Activate()
     }
     $_c = [LocalSTT].config;
-    $_t = [IO.Path]::Combine(($_c.Stt.Script | Split-Path), "transcribe.py"); $dir = $_c.Stt.workingDirectory
+    $_t = [IO.Path]::Combine(($_c.Server.Script | Split-Path), "transcribe.py"); $dir = $_c.Server.workingDirectory
     $Process = Start-Process -FilePath "python" -ArgumentList "$_t --inputfile `"$inputFile`" --outfile `"$outFile`" --working-directory `"$dir`"" -WorkingDirectory $dir -PassThru -NoNewWindow;
     $Process.WaitForExit()
     $process.Kill(); $Process.Dispose()
     return [IO.File]::ReadAllText($outFile)
-  }
-  static [bool] ResolveRequirements() {
-    return [LocalSTT]::ResolveRequirements([LocalSTT]::data.Stt.requirementsfile, [LocalSTT]::data, $false)
-  }
-  static [bool] ResolveRequirements([string]$req_txt, [PsRecord]$config, [bool]$throwOnFail) {
-    if ($config.IsRecording) { Write-Warning "LocalSTT is already recording."; if ($null -ne [LocalSTT]::recorder) { [LocalSTT]::recorder.Stop() }; }
-    $v = (Get-Variable 'VerbosePreference' -ValueOnly) -eq 'Continue'
-    $res = [IO.File]::Exists($req_txt); $req_txt_short_path = Invoke-PathShortener $req_txt
-    if (!$res) { throw "LocalSTT failed to resolve pip requirements. From file: '$req_txt_short_path'." }
-    if (!$config.Env::req.resolved) { $config.Env::req.Resolve() }
-    $v ? $(Write-Console "(LocalSTT) " -f SlateBlue -NoNewLine; Write-Console "▶︎ Found file @ /$req_txt_short_path" -f LemonChiffon) : $null
-    if ($null -eq $config -and $throwOnFail) { throw [InvalidOperationException]::new("LocalSTT config found.") };
-    if ($null -eq $config.Env) {
-      throw [InvalidOperationException]::new("No created env was found.")
-    }
-    $was_inactive = ($config.Env.State -eq "inactive") ? $([void]$config.Env.Activate(); $true) : $false
-    $v ? $(Write-Console "(LocalSTT) " -f SlateBlue -NoNewLine; Write-Console "▶︎ Resolve requirements: " -f LemonChiffon -Animate) : $null
-    [void][LocalSTT]::Run("(LocalSTT)   pip install -r $req_txt_short_path", [scriptblock]::Create("pip install -r '$req_txt'"), $throwOnFail)
-    if ($was_inactive) { $config.Env.Deactivate() }
-    return $res -and ([LocalSTT]::ErrorLog.count -eq 0)
-  }
-  static [bool] IsFirstRun() {
-    return $null -eq ([LocalSTT] | Get-Member -Name config -Type ScriptProperty)
-  }
-  static [PsRecord] GetSttData() {
-    $d = $null
-    try {
-      $d = [LocalSTT]::GetSttData((Resolve-Path .).Path)
-    } catch {
-      "Failed to Load stt data" | Write-Console -f LightCoral
-      $_ | Format-List * -Force | Out-String | Write-Console -f LightCoral
-    }
-    return $d
-  }
-  static [PsRecord] GetSttData([string]$current_path) {
-    # .DESCRIPTION
-    #   Load stt configuration from json or toml file
-    $1strun = [LocalSTT]::IsFirstRun();
-    $mdpath = (Get-Module LocalSTT -ListAvailable -Verbose:$false).ModuleBase
-    $config = [PsRecord]@{
-      Stt             = $null
-      Env             = $null
-      Process         = ''
-      IsRecording     = $false
-      PythonVersion   = [version]'3.12.9'
-      HasRequirements = $false
-      PercentComplete = 0
-    }
-    $config.Stt = [PsRecord]@{
-      port             = 65432
-      host             = "127.0.0.1"
-      amplifyRate      = "1.0"
-      workingDirectory = $current_path
-      requirementsfile = [IO.Path]::Combine($mdpath, "Private", "requirements.txt")
-      Script           = [IO.Path]::Combine($mdpath, "Private", "stt.py")
-      outFile          = [IO.Path]::Combine($current_path, "$(Get-Date -Format 'yyyyMMddHHmmss')_output.wav")
-    }
-    $config.Stt.PsObject.Properties.Add([PSScriptproperty]::New("modulePath", [scriptblock]::Create("return `"$mdpath`""), {
-          throw [SetValueException]::new("modulePath is read-only")
-        }
-      )
-    )
-    if ($1strun) {
-      [LocalSTT].PsObject.Properties.Add([PSScriptproperty]::New("config", {
-            return [LocalSTT]::GetSttData() }, {
-            throw [SetValueException]::new("config can only be imported or edited")
-          }
-        )
-      )
-    }
-    $config.Env = New-venv -Path $current_path -Verbose:$false
-    $cfactivity = "(LocalSTT)   Set local python version to {0}" -f $config.PythonVersion
-    [void][LocalSTT]::Run($cfactivity, { param($c) [PyenvHelper]::useversion($c.PythonVersion, $c) }, $cfactivity, $config)
-    $config.HasRequirements = $1strun ? [LocalSTT]::ResolveRequirements($config.Stt.requirementsfile, $config, $false) : $false
-    return $config
   }
 }
 
