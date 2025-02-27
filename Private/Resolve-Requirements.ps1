e5c0d5cd1cfd6abd32fb4b9f5603480c255a7cc9
alain <alainQtec@gmail.com>
2025-02-27 11:37:34 +0200

    Update Resolve-Requirements.ps1
    Add pip auto upgrade

--- Private/Resolve-Requirements.ps1
+++ Private/Resolve-Requirements.ps1
@@ -2,7 +2,7 @@
   [CmdletBinding()][OutputType([bool])]
   param (
     [Parameter(Mandatory = $false, Position = 0)]
-    [string]$req_txt = [LocalSTT]::data.Server.requirementsfile,
+    [string]$reqfile = [LocalSTT]::data.Server.requirementsfile,
 
     [Parameter(Mandatory = $false, Position = 1)]
     [PsRecord]$config = [LocalSTT]::data,
@@ -12,10 +12,10 @@
   process {
     if ($config.IsRecording) { Write-Warning "LocalSTT is already recording."; if ($null -ne [LocalSTT]::recorder) { [LocalSTT]::recorder.Stop() }; }
     $v = [ProgressUtil]::data.ShowProgress
-    $res = [IO.File]::Exists($req_txt); $req_txt_short_path = Invoke-PathShortener $req_txt
-    if (!$res) { throw "LocalSTT failed to resolve pip requirements. From file: '$req_txt_short_path'." }
+    $res = [IO.File]::Exists($reqfile); $reqfile_short_path = Invoke-PathShortener $reqfile
+    if (!$res) { throw "LocalSTT failed to resolve pip requirements. From file: '$reqfile_short_path'." }
     if (!$config.Env::req.resolved) { $config.Env::req.Resolve() }
-    $v ? $(Write-Console "(LocalSTT) " -f SlateBlue -NoNewLine; Write-Console "▶︎ Found file @ / $req_txt_short_path" -f LemonChiffon) : $null
+    $v ? $(Write-Console "(LocalSTT) " -f SlateBlue -NoNewLine; Write-Console "▶︎ Found file @ / $reqfile_short_path" -f LemonChiffon) : $null
     if ($null -eq $config -and $throwOnFail) { throw [InvalidOperationException]::new("LocalSTT config found.") };
     if ($null -eq $config.Env) {
       throw [InvalidOperationException]::new("No created env was found.")
@@ -25,8 +25,13 @@
       param([PsRecord]$cfg, [string]$reqtxt, [switch]$throw)
       $was_inactive = $cfg.Env.State -eq "inactive"
       try {
+        [IO.FileInfo]$pip = "/$(Get-Variable HOME -ValueOnly)/.pyenv/shims/pip"
+        if (!$pip.Exists) {
+          throw [IO.FileNotFoundException]::new("pip shim not found. Install pyenv and try again.")
+        }
         if ($was_inactive) { [void]$cfg.Env.Activate() }
-        & "/$(Get-Variable HOME -ValueOnly)/.pyenv/shims/pip" install -r $reqtxt
+        & $pip.FullName install --upgrade pip
+        & $pip.FullName install -r $reqtxt
       } catch {
         [LocalSTT]::LogErrors($_);
         if ($throw) {
@@ -38,8 +43,8 @@
         if ($was_inactive) { [void]$cfg.Env.Deactivate() }
       }
     }
-    $msg = "(LocalSTT)   pip install -r $req_txt_short_path"
-    [void][LocalSTT]::Run($msg, $s, ($config, $req_txt, $throwOnFail))
+    $msg = "(LocalSTT)   pip install -r $reqfile_short_path"
+    [void][LocalSTT]::Run($msg, $s, ($config, $reqfile, $throwOnFail))
     $res = $res -and ([LocalSTT]::ErrorLog.count -eq 0)
   }
 
