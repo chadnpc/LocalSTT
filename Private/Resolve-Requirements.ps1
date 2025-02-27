cdc7aa8a29c27dfaf6af29f2d42fb7df4efbe4e7
alain <alainQtec@gmail.com>
2025-02-27 11:26:17 +0200

    Update Resolve-Requirements.ps1

--- Private/Resolve-Requirements.ps1
+++ Private/Resolve-Requirements.ps1
@@ -15,15 +15,31 @@
     $res = [IO.File]::Exists($req_txt); $req_txt_short_path = Invoke-PathShortener $req_txt
     if (!$res) { throw "LocalSTT failed to resolve pip requirements. From file: '$req_txt_short_path'." }
     if (!$config.Env::req.resolved) { $config.Env::req.Resolve() }
-    $v ? $(Write-Console "(LocalSTT) " -f SlateBlue -NoNewLine; Write-Console "▶︎ Found file @ /$req_txt_short_path" -f LemonChiffon) : $null
+    $v ? $(Write-Console "(LocalSTT) " -f SlateBlue -NoNewLine; Write-Console "▶︎ Found file @ / $req_txt_short_path" -f LemonChiffon) : $null
     if ($null -eq $config -and $throwOnFail) { throw [InvalidOperationException]::new("LocalSTT config found.") };
     if ($null -eq $config.Env) {
       throw [InvalidOperationException]::new("No created env was found.")
     }
-    $was_inactive = ($config.Env.State -eq "inactive") ? $([void]$config.Env.Activate(); $true) : $false
     $v ? $(Write-Console "(LocalSTT) " -f SlateBlue -NoNewLine; Write-Console "▶︎ Resolve requirements: " -f LemonChiffon -Animate) : $null
-    [void][LocalSTT]::Run("(LocalSTT)   pip install -r $req_txt_short_path", [scriptblock]::Create("pip install -r '$req_txt'"), $throwOnFail)
-    if ($was_inactive) { [void]$config.Env.Deactivate() }
+    $s = {
+      param([PsRecord]$cfg, [string]$reqtxt, [switch]$throw)
+      $was_inactive = $cfg.Env.State -eq "inactive"
+      try {
+        if ($was_inactive) { [void]$cfg.Env.Activate() }
+        & "/$(Get-Variable HOME -ValueOnly)/.pyenv/shims/pip" install -r $reqtxt
+      } catch {
+        [LocalSTT]::LogErrors($_);
+        if ($throw) {
+          throw $_
+        } else {
+          Write-Console $_.Exception.Message -f LightCoral
+        }
+      } finally {
+        if ($was_inactive) { [void]$cfg.Env.Deactivate() }
+      }
+    }
+    $msg = "(LocalSTT)   pip install -r $req_txt_short_path"
+    [void][LocalSTT]::Run($msg, $s, ($config, $req_txt, $throwOnFail))
     $res = $res -and ([LocalSTT]::ErrorLog.count -eq 0)
   }
 
